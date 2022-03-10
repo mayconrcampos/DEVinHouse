bd3429a58158950d4343888e3bd76c97fa263ffb
mayconrcampos <maycon.campos@gmail.com>
2022-03-10 19:29:24 -0300

    Mudando a cor de fundo usando o Dom

---
+++ SEMANA 02/Aula 04/Exemplo01/script.js
@@ -0,0 +1,7 @@
+function mudaCor(){
+    if(document.body.style.backgroundColor == "white"){
+        document.body.style.backgroundColor = "skyblue"
+    }else{
+        document.body.style.backgroundColor = "white"
+    }
+}
