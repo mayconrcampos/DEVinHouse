34a23320165258179bfa280f967baf9a52d63514
mayconrcampos <maycon.campos@gmail.com>
2022-03-10 10:54:48 -0300

    Ex05 - Implementação de um modal com mensagem amigável

---
+++ SEMANA 02/Exercicios S02/js/script.js
@@ -0,0 +1,38 @@
+function mensagem(){
+    var nome = document.getElementById("nome").value 
+    var email = document.getElementById("email").value 
+    var modal = document.getElementById("modal")
+    var modalNome = document.getElementById("modalNome")
+    var modalEmail = document.getElementById("modalEmail")
+    var formulario = document.getElementById("form")
+    var sobre = document.getElementById("sobre")
+
+    if(nome.length > 0 && email.length > 0){
+        modal.style.display = "block"
+        formulario.style.display = "none"
+        sobre.style.display = "none"
+        modalNome.innerText = `Nome: ${nome}`
+        modalEmail.innerText = `Email: ${email}`
+
+    }else{
+        
+        alert("ERRO! Digite seu nome e email.")
+    }
+}
+
+
+function fechar(){
+    var modal = document.getElementById("modal")
+    var formulario = document.getElementById("form")
+    var sobre = document.getElementById("sobre")
+
+    var nome = document.getElementById("nome")
+    var email = document.getElementById("email")
+
+    modal.style.display = "none"
+    formulario.style.display = "block"
+    sobre.style.display = "block"
+
+    nome.value = ""
+    email.value = ""
+}
