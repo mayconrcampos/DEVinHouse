06fdbb2a89ca8572051bbf5987ffbc9b042d036e
mayconrcampos <maycon.campos@gmail.com>
2022-03-10 20:08:42 -0300

    Exemplo03 - eventos addEventListener

---
+++ SEMANA 02/Aula 04/Exemplo03/script.js
@@ -0,0 +1,25 @@
+var area = document.getElementById("area")
+
+area.addEventListener("click", function clicar(){
+    area.style.backgroundColor = "black"
+    area.innerText = "Um clique"
+    area.style.color = "white"
+})
+
+area.addEventListener("mouseenter", function mouseEntra(){
+    area.style.backgroundColor = "yellow"
+    area.innerText = "Mouse entrou"
+    area.style.color = "black"
+})
+
+area.addEventListener("mouseleave", function mouseSai(){
+    area.style.backgroundColor = "beige"
+    area.innerText = "Mouse Saiu"
+    area.style.color = "brown"
+})
+
+area.addEventListener("dblclick", function duploClique(){
+    area.style.backgroundColor = "skyblue"
+    area.innerText = "Duplo clique"
+})
+
