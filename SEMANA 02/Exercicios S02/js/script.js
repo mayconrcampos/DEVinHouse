c1ead756f787bfae7d1eca9741745323f2a8b4bc
mayconrcampos <maycon.campos@gmail.com>
2022-03-10 11:21:53 -0300

    Ex05 - Ajustando mensagem amigável e contornando efeito colateral ao clicar em button ... foi substituido button por h2 pra não dar reload na página.

--- SEMANA 02/Exercicios S02/js/script.js
+++ SEMANA 02/Exercicios S02/js/script.js
@@ -1,18 +1,22 @@
 function mensagem(){
     var nome = document.getElementById("nome").value 
     var email = document.getElementById("email").value 
+
     var modal = document.getElementById("modal")
     var modalNome = document.getElementById("modalNome")
     var modalEmail = document.getElementById("modalEmail")
+
     var formulario = document.getElementById("form")
     var sobre = document.getElementById("sobre")
 
+
     if(nome.length > 0 && email.length > 0){
+        modalNome.innerText = `Nome: ${nome}`
+        modalEmail.innerText = `Email: ${email}`
         modal.style.display = "block"
         formulario.style.display = "none"
         sobre.style.display = "none"
-        modalNome.innerText = `Nome: ${nome}`
-        modalEmail.innerText = `Email: ${email}`
+       
 
     }else{
         
@@ -35,4 +39,5 @@ function fechar(){
 
     nome.value = ""
     email.value = ""
+
 }
