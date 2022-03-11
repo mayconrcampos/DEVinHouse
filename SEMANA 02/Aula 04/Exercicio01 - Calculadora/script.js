dd0a3fa8cecbddf0b5e7498e8e3a49bba2109d35
mayconrcampos <maycon.campos@gmail.com>
2022-03-10 22:28:36 -0300

    Estilizando a calculadora

--- SEMANA 02/Aula 04/Exercicio01 - Calculadora/script.js
+++ SEMANA 02/Aula 04/Exercicio01 - Calculadora/script.js
@@ -1,17 +1,16 @@
 function Calcula(){
     var primeiro = document.getElementById("primeiro")
     var segundo = document.getElementById("segundo")
+
     var multiplica = document.getElementById("multiplica")
     var divide = document.getElementById("divide")
     var soma = document.getElementById("soma")
     var subtrai = document.getElementById("subtrai")
-    var operador = document.getElementsByName("operador")
+
     var resultado = document.getElementById("resultado")
+  
 
-    console.log(multiplica.checked)
-    
-
-
+    if(primeiro.value != "" && segundo.value != ""){
         if(multiplica.checked == true ){
           
             var res = eval(`${primeiro.value} ${multiplica.value} ${segundo.value}`)
@@ -20,24 +19,31 @@ function Calcula(){
             segundo.value = ''
 
         }else if(divide.checked == true){
+
             var res = eval(`${primeiro.value} ${divide.value} ${segundo.value}`)
             resultado.innerText = `${primeiro.value} / ${segundo.value} = ${res}`
             primeiro.value = ''
             segundo.value = ''
 
         }else if(soma.checked == true){
+
             var res = eval(`${primeiro.value} ${soma.value} ${segundo.value}`)
             resultado.innerText = `${primeiro.value} + ${segundo.value} = ${res}`
             primeiro.value = ''
             segundo.value = ''
 
         }else{
+
             var res = eval(`${primeiro.value} ${subtrai.value} ${segundo.value}`)
         
             resultado.innerText = `${primeiro.value} - ${segundo.value} = ${res}`
             primeiro.value = ''
             segundo.value = ''
         }
+    }else{
+        resultado.innerHTML = "ERRO! Preencha os Campos antes de calcular!"
+    }
+        
         
     
 }
