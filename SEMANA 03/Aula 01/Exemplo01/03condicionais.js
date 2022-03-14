60e9c3db5d4d157645e1e35d41176dd4229f6d66
mayconrcampos <maycon.campos@gmail.com>
2022-03-14 20:52:00 -0300

    condicionais

---
+++ SEMANA 03/Aula 01/Exemplo01/03condicionais.js
@@ -0,0 +1,16 @@
+// Revendo fundamentos - if - else if - else
+
+var idade = 69
+
+if(idade < 18){
+    console.log(`É de menor`)
+}else if(idade >= 18 && idade < 40){
+    console.log("Praticamente uma criança")
+}else if(idade >= 40 && idade < 65){
+    console.log("Tá começando a crescer.")
+}else{
+    console.log("Tá velho")
+}
+
+
+console.log(18 >= 19)
