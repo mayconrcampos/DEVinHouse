a12fc732c64373949b1cf066272986c0b976171d
mayconrcampos <maycon.campos@gmail.com>
2022-03-14 22:10:00 -0300

    laços de repetição - a galera toda

--- SEMANA 03/Aula 01/Exemplo01/01vetores.js
+++ SEMANA 03/Aula 01/Exemplo01/01vetores.js
@@ -20,14 +20,9 @@ var nomeIdade = {
 console.log(nomeIdade.nome, nomeIdade.idade)
 console.log(carros.length)
 
-for (const carro of carros) {
-    var belina = carro == "Belina" ? "Tem belina" : "Não tem belina"
-    console.log(carro, belina)
-}
 
-for (const key in nomeIdade) {
-    console.log(nomeIdade[key], key)
-}
+
+
 
 
 // while
@@ -38,3 +33,34 @@ while(i < carros.length){
     console.log(carros[i])
     i++
 }
+
+// do while
+var j = 0
+do {
+    console.log(j)
+    j++
+    
+} while (j <= 10);
+
+// For simples
+
+for (let i = 0; i < carros.length; i++) {
+    console.log(carros[i]);
+    
+}
+
+// For in
+for (const key in carros) {
+    console.log(`Forin: ${carros[key]} ${key}`)
+}
+
+// For of
+for (const carro of carros) {
+    var belina = carro == "Belina" ? "Tem belina" : "Não tem belina"
+    console.log(`Forof: ${carro}, ${belina}`)
+}
+
+// For each
+carros.forEach(element => {
+    console.log(`foreach: ${element}`)
+});
