e830807f5fb74514c9746fc64f21be681960117f
mayconrcampos <maycon.campos@gmail.com>
2022-03-14 21:18:18 -0300

    switch case

---
+++ SEMANA 03/Aula 01/Exemplo01/04switch.js
@@ -0,0 +1,20 @@
+// Fundamentos de javascript - switch case
+
+var num = 3
+
+switch (num) {
+    case 0:
+        console.log('zero');
+        break;
+    case 1:
+        console.log('um');
+        break;
+    case 2:
+        console.log('dois');
+        break;
+    case 3:
+        console.log('três');
+        break;
+    default:
+        console.log('diferente de 0, 1, 2 e 3');
+    }
