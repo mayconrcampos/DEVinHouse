4618a767506839b642c05ed352f3869451a7c247
mayconrcampos <maycon.campos@gmail.com>
2022-03-16 15:33:50 -0300

    ex10 - carregando da localstorage

---
+++ SEMANA 03/Exercicios S03/Ex10 - Carregando a lista/script.js
@@ -0,0 +1,106 @@
+
+var btn = document.getElementById("btn")
+var item = document.getElementById("item")
+var compras = []
+
+var listaDeCompras = document.getElementById("ul")
+var salvar = document.getElementById("btn-salvar")
+var carregar = document.getElementById("btn-carregar")
+
+
+function onLoad(){
+    var meusItens = localStorage.getItem("itens")
+    var listaString = JSON.parse(meusItens)
+    compras = []
+    for (const item of listaString) {
+        compras.push({"item": item.item})
+    }
+
+    listar(compras, listaDeCompras)
+
+    alert("Lista carregada da localStorage")
+}
+    
+
+
+
+
+
+btn.addEventListener("click", () => {
+    if(item.value != ""){
+        
+        // Esvaziando lista
+        listaDeCompras.innerHTML = ""
+
+        // Inserindo item a lista de compras
+        compras.push({"item":item.value})
+
+        // Chamando função listar para renderizar lista na tela
+        listar(compras, listaDeCompras)
+
+        item.value = ""
+        item.focus()
+        
+        
+    }else{
+        alert("ERRO! Você deve preencher o campo.")
+    }
+    
+})
+
+salvar.addEventListener("click", () => {
+    var listaJson = JSON.stringify(compras)
+    listaJson = JSON.parse(listaJson)
+    console.log(listaJson)
+    localStorage.setItem("itens", JSON.stringify(listaJson))
+    
+    alert("Lista salva na localStorage")
+    
+    
+
+})
+
+carregar.addEventListener("click", () => {
+    var meusItens = localStorage.getItem("itens")
+
+    if(meusItens != null){
+        if(compras.length == 0){
+            var listaString = JSON.parse(meusItens)
+            compras = []
+            for (const item of listaString) {
+                compras.push({"item": item.item})
+            }
+        
+            listar(compras, listaDeCompras)
+        
+            alert("Lista carregada da localStorage")
+        }else{
+            alert("ERRO! Lista de compras precisa estar vazia para importar do localStorage")
+        }
+        
+    }else{
+        alert("LocalStorage vazia")
+    }
+})
+
+
+function listar(lista, ul){
+    
+    for (const index in lista) {
+        ul.innerHTML += `<li>${lista[index].item} <button onclick='deleteItem(${index})'>X</button></li>`
+    }
+}
+
+function deleteItem(index){
+    ul.innerHTML = ""
+    console.log(compras)
+    console.log(index)
+    compras.splice(index, 1)
+
+    listar(compras, listaDeCompras)
+    item.focus()
+
+}
+
+
+
