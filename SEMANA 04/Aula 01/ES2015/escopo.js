a1e97e710e7b06739a2bef0f3080607f79969053
mayconrcampos <maycon.campos@gmail.com>
2022-03-21 20:20:16 -0300

    diferenças de const e let

--- SEMANA 04/Aula 01/ES2015/escopo.js
+++ SEMANA 04/Aula 01/ES2015/escopo.js
@@ -59,3 +59,67 @@ if(altura > 90){
 
 console.log("altura fora: "+altura)
 console.log(largura)
+
+// outro exemplo
+
+console.log("-=-=-=-=-=-=-=-=-=-=-=--")
+let numero = 10
+let num = 20
+
+if(num > 10){
+    let numero = 11
+    console.log(numero)
+}
+
+console.log(numero)
+
+
+// outro exemplo em escopo de função
+
+let testeFora = 10
+
+function exemplo(){
+    const teste = 5
+
+    testeFora = 11
+
+    return teste
+}
+
+console.log(exemplo(), testeFora)
+
+
+// outro exemplo
+console.log("-=-=-=-=-=-=-=-=-=-============")
+var a 
+a = 5
+
+let b 
+b = a + 5
+
+console.log(a, b)
+
+console.log("Const com arrays =-=-=-=-=-=-=-=")
+
+const meuArray = []
+
+meuArray.push("Maycon")
+meuArray.push("Ariana")
+
+console.log(meuArray)
+
+// outro exemplo
+
+const aaaa = 10
+
+console.log(a)
+
+
+//aaaa = 11
+
+//console.log(aaaa) // erro
+
+
+// 
+
+
