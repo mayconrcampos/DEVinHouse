b35a2fd55f4562d94421f59c8966a8102ae018d9
mayconrcampos <maycon.campos@gmail.com>
2022-03-22 11:49:14 -0300

    Ex01 - Conceituando POO

---
+++ SEMANA 04/Exercicios S04/Ex01 - Conceituando POO.js
@@ -0,0 +1,25 @@
+/**
+ * Conceitue Programação Estruturada e Programação Orientada a Objetos e
+identifique sua principal diferença.
+ */
+
+/**
+ * Programação Estruturada, ou Procedural é um dos paradigmas de programação, ou seja, é uma forma de escrever códigos onde todo o fluxo da aplicação passa por três estruturas:
+sequência, onde as tarefas são realizadas uma após a outra.
+decisão, onde uma tarefa só é executada após um teste lógico, e laços de repetição, onde após teste lógico o código pode ser repetido N vezes.
+
+Na prática, esse paradigma orienta os programadores a escreverem seus códigos de forma modular com a criação de estruturas simples utilizando rotinas, sub-rotinas e funções, organizando sua execução de forma sequencial. Era a forma mais popular de escrever programas até o surgimento da POO. Também é o paradigma onde todo estudante aprende a programação por ser mais intuitivo seu aprendizado.
+
+As vantagens da programação Estruturada: É a mais fácil de aprender e também para resolver certos tipos de problemas como aqueles mais simples, mas podendo resolver também aqueles mais complexos.
+
+Na POO, o programador traz características de objetos da vida real para o código para resolver problemas. Cada objeto terá suas características e ações que ele poderá realizar, levando-se em conta a abstração, que é a capacidade de somente utilizar apenas as características que interessam para a utilização na resolução de um certo problema. Os objetos são criados usando as classes, que nada mais é que moldes, uma fôrma onde dali poderão ser criados um ou mais objetos daquela classe.
+
+Portanto, cada classe vai possuir atributos, que são as características daquela classe, e também os métodos, que são as ações que aquela classe poderá realizar.
+
+A meu ver, a POO tem as seguintes vantagens, como a questão do encapsulamento de dados a qual pode tornar acessível ou inacessível seu acesso por outras classes, também tem as vantagens da reutilização de código, a capacidade de podermos estruturar grandes programas em diversas camadas para uma melhor organização do código. Podemos separar as entidades colocando cada uma em sua classe e chamando seus métodos sempre que precisar.
+
+A POO foi adotada pelo mercado principalmente na criação de grandes projetos, por tornar possível lidarmos com grandes complexidades de código de uma forma mais organizada, tornando a sua manutenção mais facilitada do que era num sistema escrito de forma estruturada.
+
+As desvantagens, a meu ver, é que a POO é bem mais difícil de ser compreendida do que a Programação Estruturada. As estruturas do código são bem mais complexas de se escrever e contra intuitivas. Conceitos como Abstração, Encapsulamento, Visibilidade, Herança, Associação, Agregação, Composição, Polimorfismo, Interfaces, etc ... São conceitos difíceis de digerir pela maioria dos estudantes, onde eu também me incluo. Só conseguimos compreender após um bom tempo estudando todos os seus conceitos pra saber o que cada um faz e serve na prática, para isto temos de digitar muitas linhas de código pra descobrir a sua utilidade. Mas depois que sua mente digere a POO, as coisas se tornam bem mais fáceis de se realizar.
+
+ */
