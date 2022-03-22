9544fa13c4706c0f8940ea6ca125a66c1276f94f
mayconrcampos <maycon.campos@gmail.com>
2022-03-22 11:54:07 -0300

    Ex04 - Definições

---
+++ SEMANA 04/Exercicios S04/Ex04 - Definicoes.js
@@ -0,0 +1,15 @@
+/**
+ * Defina Closure, Herança, Encapsulamento e Polimorfismo.
+ */
+
+/**
+ * Closure: é uma função que se lembra do seu escopo léxico em que ela foi criada. Isto faz com que em funções aninhadas possam ter acesso às variáveis que foram declaradas no escopo externo.
+
+Herança: É a capacidade de uma Classe herdar atributos e métodos de outra classe. A classe principal é a SuperClasse ou classe Mãe, e a classe que herdou se torna uma SubClasse, ou classe filha. Podemos criar mais atributos e métodos além daqueles herdados da classe mãe.
+
+Encapsulamento: É a capacidade que uma classe possui de proteger seus atributos contra modificações externas, fazendo isso mudando seu nível de acesso. São três os níveis de acesso que podemos setar em atributos e métodos. Public, que são acessíveis de dentro e fora da classe. Protected, que são acessíveis somente dentro da própria classe e de classes herdeiras.
+Private, é a forma mais restrita onde atributos e métodos setados como private somente serão acessíveis de dentro da própria classe. Os níveis de acesso é que possibilitam o encapsulamento.
+
+Polimorfismo: É a capacidade de sobrescrevermos nas classes filhas, métodos herdados da SuperClasse. Adaptando aquele método para a realidade da subclasse fazendo-o se comportar de forma personalizada. Porém só existe o polimorfismo quando mantemos a mesma assinatura do método. Quando modificamos a assinatura deste método, então trata-se de sobrecarga de métodos.
+
+ */
