987b1e9633d3034ee596e61e3ef3890eb0c9736a
mayconrcampos <maycon.campos@gmail.com>
2022-03-23 10:43:12 -0300

    Ex08 - Validação

--- SEMANA 04/Exercicios S04/Ex08 - Validacao/Cliente.js
+++ SEMANA 04/Exercicios S04/Ex08 - Validacao/Cliente.js
@@ -5,16 +5,89 @@ class Cliente {
     numeroCelular
 
     constructor(nome, cpf, endereco, numeroCelular){
-        this.nome = nome
-        this.cpf = cpf
-        this.endereco = endereco
-        this.numeroCelular = numeroCelular
+        this.setNome(nome)
+        this.validaCPF(cpf)
+        this.setEndereco(endereco)
+        this.setNumeroCelular(numeroCelular)
     }
 
-    validaCPF(cpf){
-        if(cpf.length > 14){
+    getNome(){
+        return this.nome
+    }
+
+    setNome(nome){
+        this.nome = nome
+    }
+
+    getEndereco(){
+        return this.endereco
+    }
+
+    setEndereco(endereco){
+        this.endereco = endereco
+    }
+
+    getNumeroCelular(){
+        return this.numeroCelular
+    }
+
+    setNumeroCelular(numero){
+        this.numeroCelular = numero
+    }
+
+    getCPF(){
+        return this.cpf
+    }
+
+    setCPF(cpf){
+        this.cpf = cpf
+    }
+
+    validaCPF(strCPF){
+        if(strCPF.length == 14){
+
+            for (const key in strCPF){
+                if(strCPF[key] == "."){
+                    strCPF = strCPF.replace(".", "")
+                }else if(strCPF[key] == "-"){
+                    strCPF = strCPF.replace("-", "")
+                }
+            }
+
+            var Soma;
+            var Resto;
+            Soma = 0;
+            if (strCPF == "00000000000") return false;
+    
+            for (let i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+            Resto = (Soma * 10) % 11;
+    
+            if ((Resto == 10) || (Resto == 11))  Resto = 0;
+            if (Resto != parseInt(strCPF.substring(9, 10)) ){
+                
+                return false;
+            } 
+    
+            Soma = 0;
+            for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+            Resto = (Soma * 10) % 11;
+    
+            if ((Resto == 10) || (Resto == 11))  Resto = 0;
             
+            if (Resto != parseInt(strCPF.substring(10, 11) ) ){
+                
+                return false;
+            } 
+    
+            console.log(`${strCPF} é um CPF válido.`);
+
+            this.setCPF(strCPF)
+            return strCPF;
+        }else{
+            
+            return false;
         }
+        
     }
 
     imprimeDadosCliente(){
---
+++ SEMANA 04/Exercicios S04/Ex08 - Validacao/script.js
@@ -0,0 +1,72 @@
+
+
+function cpfmask(i){
+    var v = i.value;
+
+    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+       i.value = v.substring(0, v.length-1);
+       return;
+    }
+
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3 || v.length == 7) i.value += ".";
+    if (v.length == 11) i.value += "-";
+    
+} 
+
+function fonemask(i){
+    var v = i.value;
+
+
+    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+        i.value = v.substring(0, v.length-1);
+        return;
+    }
+
+    i.setAttribute("maxlength", "13");
+
+    if(v.length == 2){
+        i.value += " ";
+    }else if(v.length == 8){
+        i.value += "-";
+    }
+}
+
+function limparCadastro(){
+    nome.value = ""
+    cpf.value = ""
+    endereco.value = ""
+    celular.value = ""
+    nomeshow.innerText = ``
+    enderecoshow.innerText = ``
+    celularshow.innerText = ``
+}
+
+
+  var nome = document.getElementById("nome")
+  var cpf = document.getElementById("cpf")
+  var endereco = document.getElementById("endereco")
+  var celular = document.getElementById("celular")
+
+  var nomeshow = document.getElementById("nomeshow")
+  var cpfshow = document.getElementById("cpfshow")
+  var enderecoshow = document.getElementById("enderecoshow")
+  var celularshow = document.getElementById("celularshow")
+
+  var btn = document.getElementById("btn")
+  
+
+  btn.addEventListener("click", () => {
+    var cliente = new Cliente(nome.value, cpf.value, endereco.value, celular.value)
+
+    if(cliente.validaCPF(cpf.value)){
+        nomeshow.innerText = `Nome: ${cliente.getNome()}`
+        cpfshow.innerText = `CPF ${cliente.getCPF()} Validado.`
+        enderecoshow.innerText = `Endereço: ${cliente.getEndereco()}`
+        celularshow.innerText = `Nº Celular: ${cliente.getNumeroCelular()}`
+    }else{
+        cpfshow.innerText = `CPF ${cpf.value} Inválido`
+        limparCadastro()
+
+    }
+  })
