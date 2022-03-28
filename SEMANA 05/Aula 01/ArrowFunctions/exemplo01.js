8d8d8d98cbf60d7b22b4ccf71b7d12346f2fb6a2
mayconrcampos <maycon.campos@gmail.com>
2022-03-28 20:11:54 -0300

    foreach com arrow

--- SEMANA 05/Aula 01/ArrowFunctions/exemplo01.js
+++ SEMANA 05/Aula 01/ArrowFunctions/exemplo01.js
@@ -12,3 +12,23 @@ soma(2, 6)
 const somaTudo = (a, b) => console.log(a + b)
 
 somaTudo(12, 45)
+
+// const saudar com Arrow
+
+const saudar = () => console.log("Olá")
+
+saudar()
+
+// Arrows são utilizadas muito dentro de foreach
+
+const vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+vetor.forEach((num, index, vet) => {
+    console.log(num, vet[index])
+});
+
+// Podemos omitir os parenteses caso a função tenha apenas um parâmetro
+
+const numeroAoQuadrado = n => console.log(n**2)
+
+numeroAoQuadrado(5)
