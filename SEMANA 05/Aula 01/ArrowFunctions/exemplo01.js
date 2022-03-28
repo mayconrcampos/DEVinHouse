a4dab8181bea5e89f8207c13feff95664343de51
mayconrcampos <maycon.campos@gmail.com>
2022-03-28 20:02:47 -0300

    Exemplo básico arrow function

---
+++ SEMANA 05/Aula 01/ArrowFunctions/exemplo01.js
@@ -0,0 +1,14 @@
+/**
+ * Veio juntamente com o ES2015
+ */
+
+function soma(a, b){
+    console.log(a + b)
+}
+
+soma(2, 6)
+
+// Declaração Arrow
+const somaTudo = (a, b) => console.log(a + b)
+
+somaTudo(12, 45)
