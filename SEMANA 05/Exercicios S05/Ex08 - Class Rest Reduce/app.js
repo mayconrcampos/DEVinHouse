fb2e4fba8b3dbe444925a8ecd86f45ffedb89f85
mayconrcampos <maycon.campos@gmail.com>
2022-03-29 15:30:54 -0300

    ok

--- SEMANA 05/Exercicios S05/Ex08 - Class Rest Reduce/app.js
+++ SEMANA 05/Exercicios S05/Ex08 - Class Rest Reduce/app.js
@@ -7,4 +7,5 @@ const feijao = new Produto('feijao', 2, 12);
 const batata = new Produto('batata', 4, 8);
 const macarrao = new Produto('macarrao', 1, 5);
 
-console.log(calculaTotal(arroz, feijao, batata, macarrao))
+const total = calculaTotal(arroz, feijao, batata, macarrao)
+console.log(total)
