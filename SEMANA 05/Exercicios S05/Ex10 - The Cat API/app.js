8703b02bd8fbb99c93ff6b24264edfa81a396250
mayconrcampos <maycon.campos@gmail.com>
2022-03-29 18:57:51 -0300

    Ex10 - The Cat API

---
+++ SEMANA 05/Exercicios S05/Ex10 - The Cat API/app.js
@@ -0,0 +1,15 @@
+var img = document.getElementById("img")
+var btn = document.getElementById("btn")
+
+btn.addEventListener("click", () => {
+    getImagem()
+})
+
+
+async function getImagem(){
+    const imagem = await fetch("https://api.thecatapi.com/v1/images/search")
+    .then(response => response.json())
+    .then(json => {
+        img.src = `${json[0].url}`
+    })
+}
