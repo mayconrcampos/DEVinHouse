f53d5db42a2d1ae92d1a4c7aebad82eefa702b7b
mayconrcampos <maycon.campos@gmail.com>
2022-03-29 14:33:23 -0300

    Ex06 - Concatenar espalhando

---
+++ SEMANA 05/Exercicios S05/Ex06 - Concatenar espalhando/app.js
@@ -0,0 +1,6 @@
+import { concatena } from "./utils.js";
+
+let nums1 = [1,2,3,4]
+let nums2 = [5,6,7,8]
+console.log(concatena(nums1, nums2))
+console.log(concatena(nums1, nums2).length)
---
+++ SEMANA 05/Exercicios S05/Ex06 - Concatenar espalhando/utils.js
@@ -0,0 +1,3 @@
+export function concatena(array1, array2){
+    return [...array1, ...array2]
+}
