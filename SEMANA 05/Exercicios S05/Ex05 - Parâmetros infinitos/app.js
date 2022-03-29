f03df1949a02ccfbbceb3062f5aab6b9a1ccc05e
mayconrcampos <maycon.campos@gmail.com>
2022-03-29 14:24:39 -0300

    Ex05 - Parâmetros infinitos

--- SEMANA 05/Exercicios S05/Ex05 - Parâmetros infinitos/app.js
+++ SEMANA 05/Exercicios S05/Ex05 - Parâmetros infinitos/app.js
@@ -1,4 +1,7 @@
 import { somaTudo } from "./utils.js";
 
-const resultado = somaTudo(1, 3, 5, 6, 8, 10)
+const numeros = [1, 2, 3, 4, 5]
+var [um, dois, tres, quatro, cinco] = numeros
+
+const resultado = somaTudo(um, dois, tres, quatro, cinco)
 console.log(resultado)
