4129fa48375f5f89237a84d7871503e269df1771
mayconrcampos <maycon.campos@gmail.com>
2022-03-28 22:02:23 -0300

    funções para manipulação de arrays ... foreach, map, filter, some, every, find

--- SEMANA 05/Aula 01/Funções de Arrays/script.js
+++ SEMANA 05/Aula 01/Funções de Arrays/script.js
@@ -34,4 +34,57 @@ vetor.forEach((num) => {
 // Podemos fazer a mesma coisa, mas retornando valores de cada operação.
 // O map pega todos os returns de todas as iterações, e faz um novo array.
 const vetorEmDobro = vetor.map((n) => {return n * 2})
+
 console.log(vetorEmDobro)
+console.log(vetor)
+
+// Cria novo array apenas com itens que passaram no teste pela função fornecida.
+// Se fizer isso com map, ele simplesmente retorna um array com o resultado... true ou false... mas não retorna o valor que passou no teste.
+
+const maiorQueCinco = vetor.filter(n => n > 5)
+console.log(maiorQueCinco)
+
+const igualA5 = vetor.filter(n => n == 5)
+console.log("Ao quadrado?"+igualA5)
+
+
+const vetObj = [
+    {nome: "Batata", preco:2},
+    {nome: "Pera", preco:5},
+    {nome: "Uva", preco:9}
+]
+
+const novoVetObj = vetObj.map(n => n.preco < 5)
+
+console.log(novoVetObj)
+// [ true, false, false ]
+
+const novoVetObj2 = vetObj.filter(n => n.preco > 5)
+console.log(novoVetObj2)
+
+// Find .... procurando dentro do array
+// Retorna o primeiro elemento que satisfizer a condição.
+const procuraBatata = vetObj.find(n => n.nome == "Batata")
+console.log(procuraBatata)
+
+// every - retorna true ou false .. Testa todos os itens
+// Retorna true se todos os itens passarem no teste
+const todo = vetObj.every(p => p.preco > 1)
+console.log(todo)
+// true
+// Todos os itens custam mais que 1.
+
+// Some - retorna true se algum dos elementos passarem no teste
+const algum = vetObj.some(n => n.preco > 5)
+console.log(algum)
+// true
+// Pois teve a Uva que custa mais que 5
+
+
+// includes - Teste se item está no vetor
+
+console.log(vetor.includes(5))
+// true pois o vetor possui 5
+
+console.log(vetor.includes(66))
+// false, pois não existe 66 no vetor.
