ed602b4322b47c4c301574a62f6fcff4c4bf0209
mayconrcampos <maycon.campos@gmail.com>
2022-03-29 21:23:00 -0300

    spread operator

--- SEMANA 05/Aula 02/Operador Rest/script.js
+++ SEMANA 05/Aula 02/Operador Rest/script.js
@@ -6,6 +6,8 @@
  * Três pontos = ...
  */
 
+
+// Rest operator
 function apresenta(nome, idade, ...outros){
     console.log(`Olá ${nome}! Você tem ${idade} anos.`)
     if(outros.length > 0){
@@ -24,3 +26,20 @@ function somaTudo(...nums){
 
 
 console.log(somaTudo(1,2,3,4,5,6,7,8,9))
+
+// Spread operator
+
+var num1 = [1,2,3,4,5]
+var num2 = [6,7,8,9,10]
+var num3 = [...num1, ...num2]
+console.log(num3)
+
+
+// outro exemplo
+var maycon = ["Maycon", 39, "Programador"]
+
+function apresenta(nome, idade, prof){
+    console.log(nome, idade, prof)
+}
+
+apresenta(...maycon)
