024f605247d46d6880d8589ce7bf7fcc98cac6e0
mayconrcampos <maycon.campos@gmail.com>
2022-04-18 20:04:04 -0300

    demonstração de passagem de valores via props - de pai pra filho

---
+++ MODULO 01/SEMANA 08/Aula 01/comunicacao/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Aluno :nomeAluno="aluno" />
+
+    <input v-model="aluno" type="text">
+
+    <p>Nome no elemento pai: {{aluno}}</p>
+  </div>
+</template>
+
+<script>
+import Aluno from "./components/Aluno.vue"
+
+export default {
+ 
+  name: 'App',
+  data(){
+    return{
+      aluno: ""
+    }
+  },
+  components: {
+    Aluno
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ MODULO 01/SEMANA 08/Aula 01/comunicacao/src/components/Aluno.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Aluno - componente filho</h1>
+
+        <h5>Nome via Props: {{nomeAluno}}</h5>
+    </div>
+     
+</template>
+
+<script>
+export default {
+    name: "Aluno",
+    props: {
+      nomeAluno: {
+          type: String,
+          default: "Maycon",
+      }
+    }
+  
+}
+</script>
+
+<style>
+
+</style>
