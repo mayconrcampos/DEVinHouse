8f976b148c38b835e5c35c02bb41cf17181ea069
mayconrcampos <maycon.campos@gmail.com>
2022-04-19 17:42:10 -0300

    criado projeto e inserido componente formulário

---
+++ MODULO 01/SEMANA 07/Tarefas SEMANA 07/EX07 - Criar componente formulario/formulario/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <Formulario @mostrar="mostrar"/>
+  <div id="show">
+    <p>Nome: {{ nome }}</p>
+    <p>Idade: {{ idade }}</p>
+  </div>
+</div>
+</template>
+
+<script>
+import Formulario from "./components/Formulário.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Formulario
+  },
+  data(){
+    return{
+      nome: "",
+      idade: ""
+    }
+    
+  },
+  methods: {
+    mostrar(nomeIdade){
+      this.nome = nomeIdade[0]
+      this.idade = nomeIdade[1]
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ MODULO 01/SEMANA 07/Tarefas SEMANA 07/EX07 - Criar componente formulario/formulario/src/components/Formulário.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div id="form">
+            <label for="">Nome</label>
+            <input v-model="nome" type="text" placeholder="Digite seu nome">
+
+            <label for="">Idade</label>
+            <input v-model="idade" type="text" placeholder="Digite sua idade">
+
+            <button @click="$emit('mostrar', [nome, idade])">Cadastrar</button>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Formulario",
+    data(){
+        return{
+            nome: "",
+            idade: ""
+        }
+    }
+}
+</script>
+
+<style>
+#form {
+    width: 70%;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+}
+
+</style>
