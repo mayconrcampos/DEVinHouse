5c3c9ffccbb362f17d434f3b2fdbe259785c7300
mayconrcampos <maycon.campos@gmail.com>
2022-04-19 11:23:06 -0300

    funcionando ajustado

--- MODULO 01/SEMANA 08/Aula 01/cronometro/src/App.vue
+++ MODULO 01/SEMANA 08/Aula 01/cronometro/src/App.vue
@@ -1,14 +1,19 @@
 <template>
 <div>
+  <div id="header">
+    <h1>Cronômetro</h1>
+    <img src="./assets/relogio.png" alt="reloginho">
+  </div>
+    
     <div id="buttons">
-      <button @click.prevent="relogio()">Iniciar</button>
-      <button @click.prevent="parar()">Parar</button>
-      <button @click.prevent="zerar()">Zerar</button>
+      <button id="btn1" @click.prevent="relogio()">Iniciar</button>
+      <button id="btn2" @click.prevent="parar()">Parar</button>
+      <button id="btn3" @click.prevent="zerar()">Zerar</button>
     </div>
 
-    <Cronometro @addlista="marcarVolta" :listaVoltas="listadeVoltas" :hora="hora < 10 ? '0'+hora : hora" :minuto="minuto < 10 ? '0'+minuto : minuto" :segundos="segundos < 10 ? '0'+segundos : segundos"/>
+    <Cronometro @parar="parar" @limpatempo="limpatempo" @addlista="marcarVolta" :listaVoltas="listadeVoltas" :hora="addZero(hora)" :minuto="addZero(minuto)" :segundos="addZero(segundos)"/>
 
-    
+    {{tamanho}}
 </div>
 </template>
 
@@ -25,30 +30,43 @@ export default {
       minuto: 0,
       segundos: 0,
       contador: 0,
-      listadeVoltas: []
+      listadeVoltas: [],
+      tamanho: ""
     }
   },
+  mounted() {
+    window.addEventListener('resize', () => {
+      this.tamanho = window.innerWidth
+      console.log(this.tamanho)
+    })
+  },
   methods: {
-    addZero(tempo){
-      return tempo < 10 ? "0"+tempo : tempo
+    
+
+    limpatempo(){
+      this.listadeVoltas = []
     },
+    addZero(tempo){
+      return tempo < 10 ? "0"+Number(tempo) : `${tempo}`
+    },
+
     marcarVolta(tempo){
-      
       var volta = `${this.addZero(tempo[0])}:${this.addZero(tempo[1])}:${this.addZero(tempo[2])}`
       this.listadeVoltas.push(volta)
-      console.log(this.listadeVoltas)
     },
+
     zerar(){
       this.hora = 0
       this.minuto = 0
       this.segundos = 0
 
     },
+
     parar(){
       clearInterval(this.contador)
     },
+
     relogio(){
-      
       this.contador = setInterval(() => {
         this.segundos++
         if(this.segundos == 60){
@@ -64,9 +82,6 @@ export default {
           this.hora = 0
           this.segundos = 0
         }
-  
-
-      console.log(this.segundos)
       }, 1000)
 
       
@@ -76,12 +91,101 @@ export default {
 </script>
 
 <style>
+body {
+  background: rgb(255,255,255);
+  background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(78,198,234,1) 63%); 
+  top: 0;
+}
+#header {
+  top: 0;
+  left: 0;
+  display: flexbox;
+  flex-direction: row;
+  margin-bottom: 20px;
+  
+}
+#header h1 {
+  text-align: center;
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 3em;
+}
+#header img {
+  width: 150px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: whitesmoke;
   margin-top: 60px;
+}
+#buttons {
+  padding: 10px;
+}
+#btn1:hover{
+  background-color: #083d08;
+}
+#btn1 {
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: bold;
+  font-size: 2em;
+  border: none;
+  border-radius: 7px;
+  padding: 10px;
+  margin: 5px;
+  background-color: #329632;
+  box-shadow: 3px 4px 10px black;
+  color: whitesmoke;
+  cursor: pointer;
+}
+#btn2:hover{
+  background-color: rgb(175, 45, 22);
+}
+#btn2 {
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: bold;
+  font-size: 2em;
+  border: none;
+  border-radius: 7px;
+  padding: 10px;
+  margin: 5px;
+  background-color: tomato;
+  box-shadow: 3px 4px 10px black;
+  color: whitesmoke;
+  cursor: pointer;
+}
+#btn3:hover{
+  background-color: rgb(139, 153, 153);
+}
+#btn3 {
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: bold;
+  font-size: 2em;
+  border: none;
+  border-radius: 7px;
+  padding: 10px;
+  margin: 5px;
+  background-color: lightcyan;
+  box-shadow: 3px 4px 10px black;
+  color: black;
+  cursor: pointer;
+}
+div h1 {
+  color: black;
+}
+
+
+@media (max-width: 570px){
+  #btn1 {
+    font-size: 1em;
+  }
+  #btn2 {
+    font-size: 1em;
+  }
+  #btn3 {
+    font-size: 1em;
+  }
 }
 </style>
--- MODULO 01/SEMANA 08/Aula 01/cronometro/src/components/Cronometro.vue
+++ MODULO 01/SEMANA 08/Aula 01/cronometro/src/components/Cronometro.vue
@@ -1,15 +1,15 @@
 <template>
     <div id="cronometro">
-        <h2>Relógio</h2>
-
-        <p>{{ hora }}:{{ minuto }}:{{ segundos }}</p>
+        <h1 @dblclick="$emit('parar')">{{ hora }}:{{ minuto }}:{{ segundos }}</h1>
 
         <div v-show="listaVoltas.length > 0" id="listaVoltas">
             <ul v-for="(item, key) in listaVoltas" :key="key">
-              <li>{{key - item}}</li>
+              <li> {{key + 1}} - {{item}}</li>
             </ul>
         </div>
-        <button @click="$emit('addlista', [hora, minuto, segundos])">Marcar Volta</button>
+        <button @click="$emit('addlista', [hora, minuto, segundos])">Marcar Tempo</button>
+        <button @click="$emit('limpatempo')">Limpar</button>
+    
     </div>
 </template>
 
@@ -20,11 +20,78 @@ export default {
         hora: String,
         minuto: String,
         segundos: String,
-        listaVoltas: []
+        listaVoltas: {
+            type: Array
+        }
     }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Press+Start+2P&display=swap');
+#cronometro {
+    width: 50%;
+    margin: auto;
+    background-color: #fefefe;
+    box-shadow: 7px 7px 20px black;
+    padding: 20px;
+    border-radius: 10px;
+    margin-top: 10px;
+    margin-bottom: 50px;
+}
+
+#cronometro h1:hover{
+    color: #0a520a;
+}
+
+#cronometro h1 {
+    font-family: 'Open Sans', sans-serif;
+    font-family: 'Press Start 2P', cursive;
+    font-size: 4em;
+    color: #339933;
+    cursor: pointer;
+}
+
+
+#cronometro button:hover{
+    background-color: coral;
+    border-color: lightblue;
+    color: dimgray;
+}
+#cronometro button {
+    background-color: #a4a4a4;
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 2em;
+    font-weight: bold;
+    color: whitesmoke;
+    padding: 10px;
+}
+ul li {
+    list-style: none;
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 3em;
+    color: dimgray;
+}
+
+@media (max-width: 1076px){
+  #cronometro h1{
+    font-size: 2em;
+   }
+   ul li {
+       font-size: 2em;
+   }
+}
+
+@media (max-width: 570px){
+    #cronometro h1{
+    font-size: 1em;
+    }
+    ul li {
+        font-size: 1em;
+    }
+    #cronometro button {
+        font-size: 1em;
+    }
+}
 
 </style>
