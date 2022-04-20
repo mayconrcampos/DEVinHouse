1440b1e7f670dce2570833749d2fbd7b9cc5d38c
mayconrcampos <maycon.campos@gmail.com>
2022-04-20 18:02:28 -0300

    Ex10 - Chamando componente em app.js

---
+++ MODULO 01/SEMANA 07/Tarefas SEMANA 07/ex10-chamada-de-componente/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ComponenteFilho/>
+  </div>
+</template>
+
+<script>
+import ComponenteFilho from "./components/ComponenteFilho.vue"
+export default {
+  name: 'App',
+  components: {
+    ComponenteFilho
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ MODULO 01/SEMANA 07/Tarefas SEMANA 07/ex10-chamada-de-componente/src/components/ComponenteFilho.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Olá, sou o componente filho!</h1>
+</template>
+
+<script>
+export default {
+    name: "ComponenteFilho"
+}
+</script>
+
+<style>
+
+</style>
