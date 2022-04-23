3c3063ac1bbe710f527dfc735b4b6c58a7b0cdb4
mayconrcampos <maycon.campos@gmail.com>
2022-04-22 22:25:26 -0300

    lista de compras com modal

--- MODULO 01/SEMANA 08/Aula 04/exemploCom-VeeValidate/validaformulario/src/components/Formulario.vue
+++ MODULO 01/SEMANA 08/Aula 04/exemploCom-VeeValidate/validaformulario/src/components/Formulario.vue
@@ -1,40 +1,57 @@
 <template>
 <div class="container text-left">
-    <h1>Formulário</h1>
-    <Form>
+     <!-- Full screen modal -->
+<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <h1>Adicionar Itens</h1>
         <div class="mb-3">
             <label for="nomeproduto" class="form-label">Nome do produto</label>
-            <Field :rules="validaNome" name="nome" v-model="produto.nome" type="text" class="form-control" id="nomeproduto" aria-describedby="emailHelp"/>
+            <input name="nome" v-model="produto.nome" type="text" class="form-control w-75 m-auto" id="nomeproduto" aria-describedby="emailHelp"/>
             <span>
-                <ErrorMessage name="nome"/>
+                {{msgErro}}
             </span>
 
         </div>
         <div class="mb-3">
             <label for="valorproduto" class="form-label">Valor</label>
-            <Field :rules="validaValor"  name="valor" v-model="produto.valor" type="text" class="form-control" id="valorproduto"/>
-            <span>
-                <ErrorMessage name="valor"/>
-            </span>
+            <input  name="valor" v-model="produto.valor" type="text" class="form-control w-75 m-auto" id="valorproduto"/>
+            {{msgErro}}
+           
         </div>
 
-          <button @click.prevent="add()" type="submit" class="btn btn-primary" :disabled="habilitabtn">Submit</button>
-    </Form>
-      <ListaProdutos :produtos="produtos" />
+          <button @click.prevent="add()" type="submit" class="btn btn-primary w-50 m-auto" :disabled="habilitabtn">Cadastrar</button>
+      
+      <div class="modal-footer">
+        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal" data-bs-dismiss="modal">Fecha formulário</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <a class="btn btn-primary mt-5" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Abrir formulário</a>
+    
+
+
+      <ListaProdutos :produtos="produtos" :total="total" @del="del" />
+
+      
 </div>
 </template>
 
 <script>
-import {Form, Field, ErrorMessage} from "vee-validate"
 import ListaProdutos from "./ListaProdutos.vue"
+
+
 
 export default {
     name: "Formulario",
     components: {
-        ListaProdutos,
-        Form,
-        Field,
-        ErrorMessage
+        ListaProdutos
     },
     data() {
         return {
@@ -43,35 +60,40 @@ export default {
                 valor: null
             },
             produtos: [],
-            habilitabtn: false
+            habilitabtn: false,
+            msgErro: "",
+            total: null
         }
     },
     methods: {
         add(){
-            console.log(this.validaNome(this.produto.nome))
-            console.log(this.validaValor(this.produto.valor))
-            this.produtos.push({
-                    "nome": this.produto.nome,
-                    "valor": this.produto.valor
-            })
-        },
-        validaNome(atributo){
-            if(!atributo){
-                this.habilitabtn = false
-                return "Campo nome não pode ser vazio"
+            this.total = 0
+            if(this.produto.nome.length > 0 && this.produto.valor > 0 && !isNaN(this.produto.valor)){
+                this.produtos.push({
+                "nome": this.produto.nome,
+                "valor": this.produto.valor
+                })
+                this.somaTudo()
+                this.msgErro = ""
+            }else{
+                this.msgErro = "Campos Vazios não são permitidos."
             }
-            this.habilitabtn = true
-            return true
-        },
-        validaValor(atributo){
-            if(typeof atributo !== "number" || atributo == 0){
-                this.habilitabtn = false
-                return "O campo valor é obrigatório."
-            }
-            this.habilitabtn = true
-            return true
             
+
+            this.produto.nome = ""
+            this.produto.valor = ""
+            
+        },
+        del(key){
+            this.produtos.splice(key, 1)
+        },
+        somaTudo(){
+            this.produtos.forEach(item => {
+                this.total += Number(item.valor)
+            });
         }
+
+       
     },
 }
 </script>
--- MODULO 01/SEMANA 08/Aula 04/exemploCom-VeeValidate/validaformulario/src/components/ListaProdutos.vue
+++ MODULO 01/SEMANA 08/Aula 04/exemploCom-VeeValidate/validaformulario/src/components/ListaProdutos.vue
@@ -1,22 +1,54 @@
 <template>
-  <div>
-    <ul v-for="(item, key) in produtos" :key="key">
-        <li>Nome: {{ item.nome }} Valor(R$): {{ item.valor }}</li>
-    </ul>
+  <div >
+        <div v-show="produtos.length == 0" class="container mt-5">
+            <h1>Lista Vazia</h1>
+        </div>
+    <table v-show="produtos.length > 0" class="table">
+        <thead>
+          <tr>
+            <th scope="col">Indice</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Valor</th>
+            <th scope="col">Ações</th>
+          </tr>
+        </thead>
+        <tbody v-for="(item, key) in produtos" :key="key">
+          <tr>
+            <th scope="row">{{key}}</th>
+            <td>{{ item.nome}}</td>
+            <td>{{ item.valor}}</td>
+            <td><button class="btn btn-primary me-2">Editar</button><button class="btn btn-danger" @click="$emit('del', key)">Deletar</button></td>
+          </tr>
+        </tbody>
+        <td></td>
+        <td>Valor</td>
+        <td>R$ {{ total }}</td>
+    </table>
+    
   </div>
+  
 </template>
 
 <script>
 export default {
     name: "ListaProdutos",
     props: {
-        produtos: []
+        produtos: Array,
+        total: Number
+    },
+ 
+  
+
+    methods: {
+        
     }
 }
 </script>
 
-<style>
-    ul li {
-        list-style: none;
-    }
+<style scoped>
+
+
+
+
+
 </style>
