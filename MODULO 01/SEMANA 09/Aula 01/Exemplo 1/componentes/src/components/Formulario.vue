3d368a081e5c9559f381c0d2c733bb78a57f5d92
mayconrcampos <maycon.campos@gmail.com>
2022-04-26 19:12:14 -0300

    ajustando espaçamento

--- MODULO 01/SEMANA 09/Aula 01/Exemplo 1/componentes/src/components/Formulario.vue
+++ MODULO 01/SEMANA 09/Aula 01/Exemplo 1/componentes/src/components/Formulario.vue
@@ -3,36 +3,45 @@
       <h2 v-text="titulo"></h2>
       
       <div class="row g-3 mt-2">
+
           <div class="col-lg-5">
               <label class="form-label" for="">Nome do Solicitante</label>
               <input class="form-control" type="text">
           </div>
+
           <div class="col-lg-3">
                 <label class="form-label" for="">Data de Reserva</label>
                 <input class="form-control" type="text">
           </div>
+
           <div class="col-lg-2">
                 <label class="form-label" for="">Hora de Entrada</label>
                 <input class="form-control" type="text">
           </div>
+
           <div class="col-lg-2">
                 <label class="form-label" for="">Horas de Reserva</label>
                 <input class="form-control" type="text">
           </div>
+
       </div>
       <div class="row g-3 mt-2">
+
           <div class="col-lg">
               <label class="form-label" for="">Placa</label>
               <input class="form-control" type="text">
           </div>
+
           <div class="col-lg">
               <label class="form-label" for="">Modelo</label>
               <input class="form-control" type="text">
           </div>
+
           <div class="col-lg">
               <label class="form-label" for="">Ano</label>
               <input class="form-control" type="number">
           </div>
+          
       </div>
   </div>
 </template>
