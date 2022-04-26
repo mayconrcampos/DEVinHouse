cee8b1157ff9423266e60e4ac00168e335bb8145
mayconrcampos <maycon.campos@gmail.com>
2022-04-26 19:28:39 -0300

    implementando formulario

--- MODULO 01/SEMANA 09/Aula 01/Exemplo 1/componentes/src/components/Formulario.vue
+++ MODULO 01/SEMANA 09/Aula 01/Exemplo 1/componentes/src/components/Formulario.vue
@@ -6,22 +6,22 @@
 
           <div class="col-lg-5">
               <label class="form-label" for="">Nome do Solicitante</label>
-              <input class="form-control" type="text">
+              <input v-model="nome" class="form-control" type="text">
           </div>
 
           <div class="col-lg-3">
                 <label class="form-label" for="">Data de Reserva</label>
-                <input class="form-control" type="text">
+                <input v-model="dataReserva" class="form-control" type="text">
           </div>
 
           <div class="col-lg-2">
                 <label class="form-label" for="">Hora de Entrada</label>
-                <input class="form-control" type="text">
+                <input v-model="horaEntrada" class="form-control" type="text">
           </div>
 
           <div class="col-lg-2">
                 <label class="form-label" for="">Horas de Reserva</label>
-                <input class="form-control" type="text">
+                <input v-model="horasDeReserva" class="form-control" type="text">
           </div>
 
       </div>
@@ -29,17 +29,17 @@
 
           <div class="col-lg">
               <label class="form-label" for="">Placa</label>
-              <input class="form-control" type="text">
+              <input v-model="placa" class="form-control" type="text">
           </div>
 
           <div class="col-lg">
               <label class="form-label" for="">Modelo</label>
-              <input class="form-control" type="text">
+              <input v-model="modelo" class="form-control" type="text">
           </div>
 
           <div class="col-lg">
               <label class="form-label" for="">Ano</label>
-              <input class="form-control" type="number">
+              <input v-model="ano" class="form-control" type="number">
           </div>
 
       </div>
