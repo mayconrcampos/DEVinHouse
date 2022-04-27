34610b77d0a29020fee2974294545c7c6bb5fd52
mayconrcampos <maycon.campos@gmail.com>
2022-04-27 19:40:06 -0300

    ex07

---
+++ MODULO 01/SEMANA 09/Exercicios S09/Ex07 - Componente Header/exercicio-07/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-show="titulo">
+      <h3>Mostrando Componente</h3>
+      <meu-header :titulo="titulo"/>
+    </div>
+
+    <input type="text" v-model="titulo">
+    
+  </div>
+</template>
+
+<script>
+import meuHeader from "./components/header.vue"
+
+export default {
+  name: 'App',
+  components: {
+    meuHeader
+  },
+  data() {
+    return {
+      titulo: ""
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ MODULO 01/SEMANA 09/Exercicios S09/Ex07 - Componente Header/exercicio-07/src/components/header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <header>
+          <h1 v-text="titulo"></h1>
+      </header>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "meuHeader",
+    props: {
+        titulo: {
+            type: String,
+            default: () => "Aqui vai o título"
+        }
+    }
+}
+</script>
+
+<style>
+header {
+    border: 1px groove red;
+}
+
+</style>
