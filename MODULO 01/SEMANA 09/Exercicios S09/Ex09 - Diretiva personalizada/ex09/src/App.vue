286893dceb4008ff6bc657ee6c008434c711f678
mayconrcampos <maycon.campos@gmail.com>
2022-04-27 21:19:16 -0300

    ok

---
+++ MODULO 01/SEMANA 09/Exercicios S09/Ex09 - Diretiva personalizada/ex09/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <meu-form :mouseentra="aumenta" @mouseover="mousemove1" @mouseleaver="mousesai1"/>
+  </div>
+</template>
+
+<script>
+import MeuForm from "./components/formulario.vue"
+
+export default {
+  name: 'App',
+  components: {
+    MeuForm
+  },
+  data() {
+    return {
+      tamanho: "",
+      aumenta: false,
+      aumenta2: false
+    
+    }
+  },
+  methods: {
+    mousemove(){
+      this.aumenta = true
+    },
+    mousesai(){
+      this.aumenta = false
+    },
+    mousemove1(){
+      this.aumenta = true
+    },
+    mousesai1(){
+      this.aumenta = false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ MODULO 01/SEMANA 09/Exercicios S09/Ex09 - Diretiva personalizada/ex09/src/components/formulario.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container border w-75 m-auto rounded-3 mt-5">
+      <h3>Formulário</h3>
+      <label class="form-label " :class="mouseentra ? 'fs-3' : ''"  @mouseenter="$emit('mousemover')" @mouseleave="$emit('mouseleaver')">Nome</label>
+      <input :value="nome" class="form-control" type="text" placeholder="Nome completo">
+
+      <label class="form-label" for="">Idade</label>
+      <input :value="idade" class="form-control" type="number" placeholder="Idade">
+
+      <button @click="$emit('add', nome, idade)" class="btn btn-primary mt-3 mb-3">Adicionar</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MeuForm",
+    props: {
+        nome: {
+            type: String
+        },
+        idade: {
+            type: Number
+        },
+        mouseentra: {
+            type: Boolean,
+            default: () => false
+        },
+        
+        tamanho: {
+            type: Boolean,
+            default: () => false
+        }
+
+    },
+  
+
+}
+</script>
+
+<style>
+
+</style>
