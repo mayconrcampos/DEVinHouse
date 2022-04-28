e6fc0bab2c4dcc462c5a83db7cec9cff2ee3fde3
mayconrcampos <maycon.campos@gmail.com>
2022-04-27 22:41:46 -0300

    tentando usar o pinia

---
+++ MODULO 01/SEMANA 09/Aula 03/ExemploPinia/exemplo-pinia/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{store.count}}</h1>
+  </div>
+</template>
+
+<script>
+import { useMainStore } from "./store/main.js"
+export default {
+  name: 'App',
+  setup() {
+    const contadorStore = useMainStore()
+
+    return {contadorStore}
+  },
+  getters: {
+    count(state){
+      return state.counter
+    }
+  },
+  computed: {
+    triplo(){
+      return this.contadorStore.counter * 3
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
