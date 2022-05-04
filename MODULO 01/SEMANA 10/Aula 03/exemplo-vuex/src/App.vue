691c263f416df14a35ea1d6c6f0d8cb7901faa7c
mayconrcampos <maycon.campos@gmail.com>
2022-05-04 19:19:58 -0300

    criação do projeto exemplo com vuex

---
+++ MODULO 01/SEMANA 10/Aula 03/exemplo-vuex/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <img alt="Vue logo" src="./assets/logo.png">
+
+        <h1>Usando Vuex</h1>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
