99173b559f4289b8066667fd4d134ae9350669fa
mayconrcampos <maycon.campos@gmail.com>
2022-05-04 21:52:00 -0300

    exemplos com vuex, mutations, actions e modularização da store

--- MODULO 01/SEMANA 10/Aula 03/exemplo-vuex/src/App.vue
+++ MODULO 01/SEMANA 10/Aula 03/exemplo-vuex/src/App.vue
@@ -4,6 +4,39 @@
 
         <h1>Usando Vuex</h1>
 
+        <div class="container w-75 m-auto border text-center">
+
+            <h1>{{$store.state.display.value}}</h1>
+
+
+            <input type="text" v-model="display">
+            <button @click="alteraDisplay()" class="btn btn-primary">Mudar display</button>
+        </div>
+
+        <h2>Actions</h2>
+
+        <div class="container">
+          <h1>{{$store.state.segundos.value}}</h1>
+
+          <button @click.prevent="dispara()" class="btn btn-primary">Disparar Segundos</button>
+          <button @click.prevent="zerar()">Zerar contagem</button>
+          <button @click.prevent="parar()">Parar</button>
+        </div>
+        <hr>
+
+        <div class="container">
+          <input type="text" v-model="nome">
+          <input type="text" v-model="idade">
+
+          <button @click.prevent="add()" class="btn btn-primary">Add</button>
+        </div>
+
+        <ul>
+          <li v-for="(p, indice) in $store.state.pessoas.pessoas" :key="indice" >Nome: {{p.nome}} Idade: {{p.idade}} <button @click="del(indice)">x</button></li>
+        </ul>
+    
+
+
     </div>
 </template>
 
@@ -11,9 +44,41 @@
 
 export default {
   name: 'App',
+  data() {
+    return {
+      display: "jeansplay",
+      nome: "",
+      idade: ""
+    }
+  },
   components: {
     
-  }
+  },
+  computed: {
+  
+
+  },
+  methods: {
+    alteraDisplay(){
+      this.$store.commit("setdisplay",this.display)
+    },
+    dispara(){
+    this.$store.dispatch("startCron")
+    },
+    zerar(){
+      this.$store.commit("setsegundos", 0)
+    },
+    add(){
+      this.$store.commit("insertPessoa", {"nome": this.nome, "idade": this.idade})
+
+      this.nome = ""
+      this.idade = ""
+    },
+    del(i){
+      this.$store.commit("delPessoa", i)
+    }
+  },
+  
 }
 </script>
 
--- MODULO 01/SEMANA 10/Aula 03/exemplo-vuex/src/main.js
+++ MODULO 01/SEMANA 10/Aula 03/exemplo-vuex/src/main.js
@@ -1,5 +1,76 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store'
+import {createStore} from 'vuex'
 
-createApp(App).use(store).mount('#app')
+
+const pessoas = {
+    state(){
+        return {
+      
+            pessoas: []
+        }
+    },
+    mutations: {
+        insertPessoa(state, pessoa){
+            state.pessoas.push(pessoa)
+        },
+        delPessoa(state, indice){
+            state.pessoas.splice(indice, 1)
+        }
+    }
+}
+
+const display = {
+    state(){
+        return {
+            value: "Display",
+        }
+    },
+    mutations: {
+        setdisplay(state, valor){
+            state.value = valor.toUpperCase()
+        },
+    }
+}
+
+const segundos = {
+    state(){
+        return {
+            value : 0
+        }
+    },
+    mutations: {
+        
+        setsegundos(state, valor){
+            state.value = valor
+        }
+    },
+    actions: {
+        startCron(context) {
+            setInterval(() => {
+                context.state.value++
+
+            }, 1000)
+        },
+      
+    },
+    getters: {
+        tempoFormatado(state){
+            state
+        }
+    }
+}
+
+
+const store = createStore({
+    modules: {
+        display,
+        segundos,
+        pessoas
+    },
+})
+
+
+const app = createApp(App)
+app.use(store)
+app.mount('#app')
