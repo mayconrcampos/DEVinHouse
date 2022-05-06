d991d4f1b41b0dc9544977c93b2a7b24efdeadc8
mayconrcampos <maycon.campos@gmail.com>
2022-05-06 18:59:55 -0300

    userStore

--- MODULO 01/SEMANA 10/Exercicios M1S10/Ex05 - Criando nosso primeiro SPA/primeiro-spa/src/store/userStore.js
+++ MODULO 01/SEMANA 10/Exercicios M1S10/Ex05 - Criando nosso primeiro SPA/primeiro-spa/src/store/userStore.js
@@ -1,11 +1,16 @@
 export default {
     state(){
         return {
-            
+            login: {
+                usuarios: []
+            },
+            token: ""
         }
     },
     mutations: {
-
+        addUsuario(state, user){
+            state.login.usuarios.push(user)
+        }
     },
     actions: {
 
