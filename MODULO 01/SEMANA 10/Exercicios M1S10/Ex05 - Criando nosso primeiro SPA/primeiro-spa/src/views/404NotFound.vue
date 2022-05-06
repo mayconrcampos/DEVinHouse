46aa178726f8e6e2931848969463cac87bf15ab2
mayconrcampos <maycon.campos@gmail.com>
2022-05-06 19:19:47 -0300

    arquivo rota error 404

---
+++ MODULO 01/SEMANA 10/Exercicios M1S10/Ex05 - Criando nosso primeiro SPA/primeiro-spa/src/views/404NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+      <h1 class="text-center mt-3">404 - Página não encontrada</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "errorPage"
+}
+</script>
+
+<style>
+
+</style>
