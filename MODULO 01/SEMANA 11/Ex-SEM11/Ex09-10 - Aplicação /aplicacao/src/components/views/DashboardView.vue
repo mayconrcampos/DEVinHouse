26340857620d9fb53615c495ba03fab54a84a1f3
mayconrcampos <maycon.campos@gmail.com>
2022-05-14 21:48:37 -0300

    Ta funcionando muito bem

--- MODULO 01/SEMANA 11/Ex-SEM11/Ex09-10 - Aplicação /aplicacao/src/components/views/DashboardView.vue
+++ MODULO 01/SEMANA 11/Ex-SEM11/Ex09-10 - Aplicação /aplicacao/src/components/views/DashboardView.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="container">
-      <h1 class="text-center">Dashboard</h1>
+    <h1 class="text-center">Dashboard</h1>
+
+    <h3>Seja muito bem vindo!</h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, magnam.
+      Consectetur unde id, modi architecto odio repellat! Placeat fugiat odit
+      dignissimos illum eveniet voluptas nihil sapiente atque neque, alias
+      dolores.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid pariatur
+      inventore aut eum nam rerum illo distinctio, sequi in, perspiciatis
+      maiores, consequuntur ducimus quae nesciunt. Molestiae nostrum obcaecati
+      inventore ullam.
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis error
+      beatae, expedita eaque, eius rerum unde voluptas autem nostrum in non
+      natus modi? Quam id aut architecto perspiciatis fuga sequi?
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-    name: "DashBoard"
-}
+  name: "DashBoard",
+};
 </script>
 
 <style>
-
 </style>
--- MODULO 01/SEMANA 11/Ex-SEM11/Ex09-10 - Aplicação /aplicacao/src/components/views/HomeView.vue
+++ MODULO 01/SEMANA 11/Ex-SEM11/Ex09-10 - Aplicação /aplicacao/src/components/views/HomeView.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="container">
-      <h1 class="text-center">Home</h1>
+    <h1 class="text-center">Home</h1>
+
+    <h2>Esta é a sua Home!!</h2>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nobis
+      voluptas facilis nesciunt, vero eum provident corporis. Saepe praesentium
+      quod, quae voluptates adipisci architecto magnam perferendis fuga dolore.
+      Saepe, amet!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas esse,
+      consectetur rerum laudantium assumenda corporis, impedit obcaecati magnam,
+      amet earum magni corrupti asperiores harum! Voluptas iste ducimus quae
+      beatae quisquam.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto dolorem
+      minima deserunt vel ea eligendi laborum fuga consectetur ut delectus quis,
+      voluptate aperiam non! Aut eius dolore maiores? Facilis, id?
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-    name: "hoMe"
-}
+  name: "hoMe",
+};
 </script>
 
 <style>
-
 </style>
